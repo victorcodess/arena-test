b192d42165f0596e1e1464dfe61e09c0328231bc
Victor Williams <victor.williams@stu.cu.edu.ng>
2023-12-14 20:03:32 +0100

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import SignUp from "./routes/sign-up.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SignUp />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />{" "}
+  </React.StrictMode>
+);
---
+++ src/routes/sign-up.tsx
@@ -0,0 +1,5 @@
+const SignUp = () => {
+  return <main className="bg-[#EEF8FF] w-full">SignUp</main>;
+};
+
+export default SignUp;
